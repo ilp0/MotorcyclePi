344bc04b8b9291c7735e710da826f599c4e19f16
Loikkanen <ilpo.loikkanen@storaenso.com>
2018-07-19 10:25:54 +0300

    +Temp Sensor Script +Dashboard Script

---
+++ dashboard_script.py
@@ -0,0 +1,70 @@
+import bmp280.py
+import datetime
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+while True:
+
+    temperature, pressure, humidity = bmp280.readBME280All()
+    now = datetime.datetime.now()
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    draw.text((x, top),     "Temperature: " + str(temperature), font=font, fill=255)
+    draw.text((x, top+8),   "Time: " + "%02s:%02s" % (now.hour, now.minute), font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+time.sleep(.1)
